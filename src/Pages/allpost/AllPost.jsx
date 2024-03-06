4162eb5c412d78ea8049a646feb296171c51f642
deepamidhun25 <deepakrishnan1993@gmail.com>
2024-03-06 10:18:50 +0530

    updated

--- src/Pages/allpost/AllPost.jsx
+++ src/Pages/allpost/AllPost.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './AllPost.css'
 import Navbar from '../navbar/Navbar'
 import Footer from '../../components/Footer/Footer'
@@ -13,8 +13,22 @@ import {
 import { FaHeart } from "react-icons/fa";
 import { FaCommentAlt } from "react-icons/fa";
 import { FaLocationDot } from "react-icons/fa6";
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
 
 function AllPost() {
+
+  const [addvalue,setComment]=useState(false)
+
+  //Add comment toggle comment box display using comment link clicks
+
+  const addComment=()=>{
+    console.log("add comment");
+    // setComment(true)
+    setComment(prevState => !prevState);
+  }
+
+
   return (
     <div className='mainBody'>
         <Navbar/>
@@ -35,14 +49,52 @@ function AllPost() {
                         <span><FaHeart className='text-danger' /> 14 Like</span>
                       </div>
                       <div className="action-item">
-                        <span><FaCommentAlt /> 4 comments</span>
+                        <span onClick={addComment}><FaCommentAlt /> 4 comments</span>
                       </div>
                     </div>
-    
+                    <div>
+          {
+           addvalue?
+
+          //  comments display
+
+          <Card className='mt-5'>
+          <Card.Body>
+            <Card.Title>Comments</Card.Title>
+            <Card.Text>
+              {/* comment enter field */}
+             <input type="text" className='input-box' placeholder='Enter your comments here..' />    
+
+            </Card.Text>
+            {/* button for comments add  */}
+
+            <Button variant="Success">Add comment</Button>
+
+            {/* button for comments delete */}
+            <Button variant="Success" className='ms-3'>Delete comment</Button>
+
+{/* display comments loaded from server */}
+            <div className='input-box mt-5 '>
+              <p>disply comments here</p>
+
+            </div>
+          </Card.Body>
+        </Card>
+            
+            
+            
+            
+            :""
+          }
+
+
+
+        </div>
 
                   </MDBCardBody>
                 </MDBCard>
         </div>
+      
         <div className="col-2"></div>
        </div>
 
