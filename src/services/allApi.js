a8d4bfeb3f28968660e735bfd86e523bc7c8b3f5
deepamidhun25 <deepakrishnan1993@gmail.com>
2024-03-06 12:55:10 +0530

    updated

--- src/services/allApi.js
+++ src/services/allApi.js
@@ -1,2 +1,15 @@
 
 
+
+import BASE_URL from "./baseurl"
+import { commonRequest } from "./CommonReq"
+
+
+//get assembli description
+// export const get_list_places=async()=>{
+//     return commonRequest("GET",`${BASE_URL}/busbooking/places/getplaces`,"")
+// }
+
+// export const getbus=async(userData)=>{
+//     return commonRequest("POST",`${BASE_URL}/busbooking/getbusdetails`,userData)
+// }
